13d13c2af58f5dd2eb18fb32f53d771f007ede54
Dexter <26281791+dextersjab@users.noreply.github.com>
2024-09-15 19:30:52 +0100

    + setup.py for use as dependency

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="workflow-graph",
+    version="0.1.0",
+    description="A lightweight package for managing workflow graphs",
+    author="Dexter Awoyemi",
+    author_email="dexter@dextersjab.xyz",
+    url="https://github.com/dextersjab/workflow-graph",
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT",
+)
