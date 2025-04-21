8d5045f806054d5dd735155115c7b6a1b3bd5f11
Dexter <26281791+dextersjab@users.noreply.github.com>
2025-04-21 17:51:24 +0100

    bump version 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="workflow-graph",
-    version="0.3.0",
+    version="0.3.1",
     description="A lightweight package for managing workflow graphs",
     author="Dexter Awoyemi",
     author_email="dexter@dextersjab.xyz",
